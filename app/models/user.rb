741d7e8fd26aefc8061d53aa676070dbb0a398dd
uem0301 <deadman-kein@ezweb.ne.jp>
2020-09-27 22:50:31 +0900

    update3_IntroducingDevise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
-
+         :recoverable, :rememberable, :validatable
+        #  :confirmable, :lockable, :timeoutable, :trackable
   validates :name, presence: true
+
+    # has_many :collections,:relationships,:items,:comments
+
 end
