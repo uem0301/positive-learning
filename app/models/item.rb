bdb03d56bfd4166c85b25ab67639e658174d392c
densetsu <tab.23.yude@gmail.com>
2020-10-18 23:56:35 +0900

    add#3_posting_function

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
   belongs_to  :collection
   mount_uploader :image, ImageUploader
   mount_uploader :video, VideoUploader
-  validates :title, presence: true, unless: :image?
-  validates :video, presence: true, unless: :video?
+  validate :image_or_video_present?
+
+  # 画像もしくは動画がない場合エラーとする
+  def image_or_video_present?
+    if image.blank? && video.blank?
+      errors.add(:error, "画像もしくは動画をアップロードしてください。")
+    end
+  end
 end
