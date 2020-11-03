3147bfcd70ac5b4049804654208bfeb7885d0cd4
uem0301 <deadman-kein@ezweb.ne.jp>
2020-11-03 22:08:37 +0900

    Update_item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,9 @@ class Item < ApplicationRecord
   validate :image_or_video_present?
   has_many :item_tags
   has_many :tags , through: :item_tags
-
+  
+  validates :title, presence: true
+  validates :explanation, presence: true
   # 画像もしくは動画がない場合エラーとする
   def image_or_video_present?
     if image.blank? && video.blank?
