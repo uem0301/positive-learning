cb98986514fda76ae1dafebfdf879799e0d1669b
uem0301 <deadman-kein@ezweb.ne.jp>
2020-11-26 19:06:18 +0900

    Update_application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:password])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name,:email,:password] )
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name] )
     end
 
 end
