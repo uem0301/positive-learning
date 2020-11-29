95a533696fa64a07ed1df52cb91225366d5f7037
uem0301 <deadman-kein@ezweb.ne.jp>
2020-11-29 14:46:11 +0900

    Controller.rb_fixes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(item_id: params[:item_id])
-    favorite.save
-    redirect_to items_path
+    if favorite.save
+      redirect_to items_path
+    end
   end
 
   def destroy
     favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
-    favorite.destroy
-    redirect_to items_path
+    if favorite.destroy
+      redirect_to items_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   get "users/show" => "users#show"
   root 'pages#index'
